8a7530cee25717ea4f971c1768d23469c1b4dbfe
zidankar <zidanekarima98@gmail.com>
2022-04-08 12:39:05 +0200

    tester fichier

--- src/main/java/fr/univrouen/rss22/model/TestRSS.java
+++ src/main/java/fr/univrouen/rss22/model/TestRSS.java
@@ -10,13 +10,12 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 public class TestRSS {
-	@Test
 	public String loadFileXML() {
 		org.springframework.core.io.Resource resource 
 		= new DefaultResourceLoader().getResource("classpath:xml/item.xml");
